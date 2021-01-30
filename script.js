e7b7d1421914de5831e98c0d6aa54dfcd687ca02
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-01-29 21:57:24 -0800

    Created style sheet and added starting variables

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 //var hello = document.querySelector("container");
 
-var times = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];  
+var times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];  
 
 //$(".container").append("<p>Hello</p>");
 var allCont = document.querySelector(".time");
+var allEnt = document.querySelector(".entry");
+var saveButton = document.querySelector(".saveButton");
 for (var i =0; i < times.length; i++){
     
     var eachTime = document.createElement("p");
-    //$("p").addClass("hour", "row");
-    eachTime.classList.add("hour", "row");
+    
+    eachTime.classList.add("hour", "row", "description");
     eachTime.textContent += times[i];
     allCont.appendChild(eachTime);
     
+    // var eachEnt = document.createElement("form");
+    // //$("p").addClass("hour", "row");
+    // eachEnt.classList.add("present", "row", "textarea", "description");
+    // //eachEnt.textContent += times[i];
+    // allEnt.appendChild(eachEnt);
+    
+    // var eachButton = document.createElement("button");
+    // //var eachImage = document.createElement("i");
+    // //eachImage.classList.add("fas fa-save");
+    // //$("p").addClass("hour", "row");
+    // eachButton.classList.add( "row", "description", "saveBtn", "saveBtn", "saveBtn", "i:hover");
+    // //eachButton.textContent = document.createElement("i").classList.add("fas fa-save") + times[i];
+    // saveButton.appendChild(eachButton);
+    
     //var eachButton = document.createElement("button");
 
 }
-
-var allEnt = document.querySelector(".entry");
-for (var i =0; i < times.length; i++){
+//Individual sections to append data
+// var allEnt = document.querySelector(".entry");
+// for (var i =0; i < times.length; i++){
     
-    var eachEnt = document.createElement("a");
-    //$("p").addClass("hour", "row");
-    eachEnt.classList.add("present");
-    eachEnt += times[i];
-    allEnt.appendChild(eachEnt);
+//     var eachEnt = document.createElement("p");
+//     //$("p").addClass("hour", "row");
+//     eachEnt.classList.add("present", "row");
+//     eachEnt.textContent += times[i];
+//     allEnt.appendChild(eachEnt);
     
-    //var eachButton = document.createElement("button");
+//     //var eachButton = document.createElement("button");
 
-}
+// }
+
+
+// for (var i =0; i < times.length; i++){
+//     var saveButton = document.querySelector(".saveButton");
+//     var eachButton = document.createElement("button");
+//     var eachImage
+//     //$("p").addClass("hour", "row");
+//     eachButton.classList.add( "saveBtn", "row");
+//     eachButton.textContent = document.createElement("i") + times[i];
+//     saveButton.appendChild(eachButton);
+    
+//     //var eachButton = document.createElement("button");
+
+// }
